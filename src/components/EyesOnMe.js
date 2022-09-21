fa8c9667a5eac2c543cd2a902ab9117941ff5560
Beulah Mathenge <beulahmathenge@gmail.com>
2022-09-21 06:58:05 +0300

    Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+// 1. In the `components/EyesOnMe.js` file, create a `EyesOnMe` React component.
+// 2. In that component, render a `button` with the text `'Eyes on me'`.
+// 3. On that `button`, add event handlers that listen for the `focus` and `blur` events.
+// 4. When the `focus` event fires, use `console.log` to print out the text `'Good!'`.
+// 5. When the `blur` event fires, use `console.log` to print out the text `'Hey! Eyes on me!'`.
+
+import React from "react";
+
+function EyesOnMe(){
+
+    function handleBlur(){
+        console.log('Hey! Eyes on me!');
+    }
+
+    function handleFocus(){
+        console.log('Good!');
+    }
+
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handlePassword(){
+        console.log("Entering password...");
+    }
+
     return (
-        <div></div>
+       <input type="password" onChange={handlePassword} />
     )
 }
 
 export default Keypad;
+
+
+// 1. In the `components/Keypad.js` file, create a `Keypad` React component.
+// 2. In that component, render an `input` with the right type.
+// 3. On that `input`, add an event handler that listens for the `change` event.
+// 4. When that event fires, use `console.log` to print out the text `'Entering password...'`.
